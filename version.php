dc50358b1459bda89d8655d5d84d4be07506ee89
micman73 <micmanolis@gmail.com>
2025-08-28 10:48:55 +0300

    Create version.php

---
+++ version.php
@@ -0,0 +1,18 @@
+    <!-- File 2: Version File -->
+    <div class="file-section">
+        <div class="file-header">
+            📁 Version File
+            <span class="file-path">blocks/myxp/version.php</span>
+        </div>
+        <pre><?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'block_myxp';
+$plugin->version = 2025010103; // Updated version for new XP system
+$plugin->requires = 2022041900; // Moodle 4.0+
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.3 - ROBUX Edition';
+</pre>
+    </div>
+
+ 
