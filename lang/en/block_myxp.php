9f0517cb6ce2e2a47940e62f91a53aa0cdd0c09d
micman73 <micmanolis@gmail.com>
2025-08-28 11:55:28 +0300

    Update block_myxp.php

--- lang/en/block_myxp.php
+++ lang/en/block_myxp.php
@@ -1,2 +1,10 @@
-$string['task_backfill'] = 'Backfill XP for past completions and quizzes';
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Strings for component 'block_myxp', language 'en'
+
+$string['pluginname'] = 'My XP Block';
+$string['xp_awarded'] = 'You have been awarded {$a} XP!';
+$string['totalxp'] = 'Total XP';
+
 
