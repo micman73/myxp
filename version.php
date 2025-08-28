9e64c1c2b1c40fd7a768083c83211d5c786f5e3d
micman73 <micmanolis@gmail.com>
2025-08-28 11:48:55 +0300

    Update version.php

--- version.php
+++ version.php
@@ -8,7 +8,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_myxp';
-$plugin->version = 2025010103; // Updated version for new XP system
+$plugin->version = 2025010104; // Updated version for new XP system
 $plugin->requires = 2022041900; // Moodle 4.0+
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.3 - ROBUX Edition';
