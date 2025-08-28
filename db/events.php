334d655ba4cef085e6887f0af6d5909b42ee0db2
micman73 <micmanolis@gmail.com>
2025-08-28 11:34:45 +0300

    Update events.php

--- db/events.php
+++ db/events.php
@@ -1,34 +1,35 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
 
- '\core\event\course_module_completion_updated',
-        'callback' => 'block_myxp_observer::course_module_completed',
+$observers = [
+    [
+        'eventname'   => '\core\event\course_module_completion_updated',
+        'callback'    => '\block_myxp\observer::activity_completed',
         'includefile' => '/blocks/myxp/classes/observer.php',
-    ),
-    
-    // Quiz attempt submitted
-    array(
-        'eventname' => '\mod_quiz\event\attempt_submitted',
-        'callback' => 'block_myxp_observer::quiz_submitted',
+    ],
+    [
+        'eventname'   => '\mod_quiz\event\attempt_submitted',
+        'callback'    => '\block_myxp\observer::quiz_submitted',
         'includefile' => '/blocks/myxp/classes/observer.php',
-    ),
-    
-    // Assignment submitted
-    array(
-        'eventname' => '\mod_assign\event\assessable_submitted',
-        'callback' => 'block_myxp_observer::assignment_submitted',
+    ],
+    [
+        'eventname'   => '\mod_quiz\event\attempt_graded',
+        'callback'    => '\block_myxp\observer::quiz_graded',
         'includefile' => '/blocks/myxp/classes/observer.php',
-    ),
-    
-    // Forum post created
-    array(
-        'eventname' => '\mod_forum\event\post_created',
-        'callback' => 'block_myxp_observer::forum_post_created',
+    ],
+    [
+        'eventname'   => '\mod_assign\event\assessable_submitted',
+        'callback'    => '\block_myxp\observer::assignment_submitted',
         'includefile' => '/blocks/myxp/classes/observer.php',
-    ),
-    
-    // Course viewed
-    array(
-        'eventname' => '\core\event\course_viewed',
-        'callback' => 'block_myxp_observer::course_viewed',
+    ],
+    [
+        'eventname'   => '\mod_forum\event\post_created',
+        'callback'    => '\block_myxp\observer::forum_post_created',
         'includefile' => '/blocks/myxp/classes/observer.php',
-    ),
-);
+    ],
+    [
+        'eventname'   => '\core\event\course_viewed',
+        'callback'    => '\block_myxp\observer::course_viewed',
+        'includefile' => '/blocks/myxp/classes/observer.php',
+    ],
+];
